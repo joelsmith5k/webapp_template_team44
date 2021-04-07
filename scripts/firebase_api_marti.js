99f2824b93378ae6b3a0116dbbb48607860bb64b
Martin Gatchev <mgatchev@gmail.com>
2021-04-07 14:26:38 -0700

    Changed swipe pages

--- scripts/firebase_api_marti.js
+++ scripts/firebase_api_marti.js
@@ -19,3 +19,59 @@ firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 // Get a reference to the storage service, which is used to create references in your storage bucket
 // const storage = firebase.storage();
+
+function readTitle(){
+  db.collection("movies").doc("Bladerunner")
+  .onSnapshot(function(c){
+      console.log ("current document data: " + c.data());                       //.data() returns data object
+      document.getElementById("displayed-movie").innerHTML = c.data().title;    //using vanilla javascript
+      //$('#quote-goes-here').text(c.data().quote);                             //using jquery object dot notation
+      //$("#quote-goes-here").text(c.data()["quote"]);                          //using json object indexing
+  })
+}
+readTitle();
+
+function writeFilms() {
+  var citiesRef = db.collection("films");
+
+  citiesRef.add({
+      title: "Ikiru",
+      director: "Akira Kurosawa",
+  });
+}
+
+writeFilms();
+
+function displayMovie(){
+  db.collection("movies")
+  .get()
+  .then(function(snap){
+      snap.forEach(function(doc){
+
+          console.log(snap)
+          
+          console.log("Going through movies.")
+
+          var movieTitle = doc.data().title;
+          var movieDirector = doc.data().director;
+          var movieYear = doc.data().year;
+          var movieDescription = doc.data().description;
+          
+          document.getElementById('displayed-movie').innerHTML = movieTitle;
+          document.getElementById('displayed-movie-small').innerHTML = movieTitle;
+          document.getElementById('displayed-director').innerHTML = movieDirector;
+          document.getElementById('displayed-year').innerHTML = movieYear;
+          document.getElementById('displayed-description').innerHTML = movieDescription ;
+          
+          console.log(movieTitle);
+          console.log(movieDirector);
+          console.log("bless")
+      })
+
+  })
+}
+displayMovie();
+
+console.log("second bless")
+
+
