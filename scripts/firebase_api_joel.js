da162a56f058ce1c0c5878de3189d6ef03a156d3
JOEL_SMITH <joelsmith5000@gmail.com>
2021-04-07 23:53:00 -0700

    implemented read from firestore to populate groups page

--- scripts/firebase_api_joel.js
+++ scripts/firebase_api_joel.js
@@ -5,18 +5,16 @@
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyBo8Z2nhjdAq9LiUJqVs2TaOKQNA3-2g0E",
-    authDomain: "cineder-3be64.firebaseapp.com",
-    projectId: "cineder-3be64",
-    storageBucket: "cineder-3be64.appspot.com",
-    messagingSenderId: "855842445281",
-    appId: "1:855842445281:web:281e3be0aa8ffb17d164a0"
-  };
-  
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  // Get a reference to database
-  const db = firebase.firestore();
-  // Get a reference to the storage service, which is used to create references in your storage bucket
-  // const storage = firebase.storage();
-  
+  apiKey: "AIzaSyBo8Z2nhjdAq9LiUJqVs2TaOKQNA3-2g0E",
+  authDomain: "cineder-3be64.firebaseapp.com",
+  projectId: "cineder-3be64",
+  storageBucket: "cineder-3be64.appspot.com",
+  messagingSenderId: "855842445281",
+  appId: "1:855842445281:web:281e3be0aa8ffb17d164a0"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// Get a reference to database
+const db = firebase.firestore();
+// Get a reference to the storage service, which is used to create references in your storage bucket
+// const storage = firebase.storage();
