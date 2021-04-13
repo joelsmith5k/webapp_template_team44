03597f37f0662528249a5ed538a5bc1bdbd5687b
Martin Gatchev <mgatchev@gmail.com>
2021-04-13 12:52:48 -0700

    added swipe and match functionality

--- scripts/matchesFunctions.js
+++ scripts/matchesFunctions.js
@@ -1,6 +1,174 @@
+/* ------------------------------------- Variables ------------------------------------- */
+
+var movies;
+var movieIndex = 0;
+
 
 /* ------------------------------ HTML Re-Direct Functions ------------------------------ */
 
-document.getElementById(id = "matchesToSwipeButton").onclick = function () {
-    location.href = "./swipe • collection.html";
+// document.getElementById(id = "matchesToSwipeButton").onclick = function () {
+//     location.href = "./swipe • collection.html";
+// };
+
+
+/* ------------------------------ Movie Display Functions ------------------------------ */
+
+function getMovies() {
+    db.collection("movies")
+        .get()
+        .then(function (snap) {
+            movies = snap.docs;
+            snap.forEach(element => {
+                addMatchesToPage(element)
+            });
+        })
+}
+
+getMovies();
+
+// async function syncMovies() {
+//     db.collection("movies")
+//         .get()
+//         .then(function (snap) {
+//             let syncedMovies = snap.docs;
+//             movies = snap.docs;
+//             console.log(movies)
+            
+//         })
+// }
+
+
+
+function addMovieToPage() {
+
+    var movieDiv = document.createElement("div");
+    movieDiv.setAttribute("class", "row align-items-center");
+
+    var movieDivTwo = document.createElement("div");
+    movieDivTwo.setAttribute("class", "match_col");
+    var imageAside = document.createElement("aside");
+    imageAside.setAttribute("class", "match_image");
+    var movieImage = document.createElement("img");
+    movieImage.setAttribute("src", "./images/ikiru.jpg")
+    imageAside.appendChild(movieImage)
+    movieDivTwo.appendChild(imageAside)
+
+    // Info for movies here
+    // Title
+    var matchedMovieTitle = document.createElement("div")
+    matchedMovieTitle.setAttribute("class", "match_text")
+    var titleHeader = document.createElement("h2")
+    var titleText = document.createTextNode("Tester Movie (1969)")
+    titleHeader.appendChild(titleText)
+
+    matchedMovieTitle.appendChild(titleHeader)
+
+    // Director
+    var matchedDirector = document.createElement("h5")
+    var textDirector = document.createTextNode("Tester Director")
+    matchedDirector.appendChild(textDirector)
+
+    matchedMovieTitle.appendChild(matchedDirector)
+
+    // Description
+    var matchedDescription = document.createElement("h6")
+    var textDescription = document.createTextNode("Tester Description")
+    matchedDescription.appendChild(textDescription)
+
+    matchedMovieTitle.appendChild(matchedDescription)
+
+
+    movieDivTwo.appendChild(matchedMovieTitle)
+    
+    movieDiv.appendChild(movieDivTwo)
+
+    document.getElementById("matches_container").appendChild(movieDiv)
+}
+
+
+function addMatchesToPage(movieVariable) {
+
+
+    var movieTitle = movieVariable.data().title;
+    var movieDirector = movieVariable.data().director;
+    var movieYear = movieVariable.data().year;
+    var movieDescription = movieVariable.data().description;
+    var movieImageURL = movieVariable.data().image_url;
+
+    console.log(movieTitle)
+
+    var movieDiv = document.createElement("div");
+    movieDiv.setAttribute("class", "row align-items-center");
+
+    var movieDivTwo = document.createElement("div");
+    movieDivTwo.setAttribute("class", "match_col");
+    var imageAside = document.createElement("aside");
+    imageAside.setAttribute("class", "match_image");
+    var movieImage = document.createElement("img");
+    movieImage.setAttribute("src", movieImageURL)
+    imageAside.appendChild(movieImage)
+    movieDivTwo.appendChild(imageAside)
+
+    // Info for movies here
+    // Title
+    var matchedMovieTitle = document.createElement("div")
+    matchedMovieTitle.setAttribute("class", "match_text")
+    var titleHeader = document.createElement("h2")
+    var titleText = document.createTextNode(movieTitle)
+    titleHeader.appendChild(titleText)
+
+    matchedMovieTitle.appendChild(titleHeader)
+
+    // Director
+    var matchedDirector = document.createElement("h5")
+    var textDirector = document.createTextNode(movieDirector)
+    matchedDirector.appendChild(textDirector)
+
+    matchedMovieTitle.appendChild(matchedDirector)
+
+    // Description
+    var matchedDescription = document.createElement("h6")
+    var textDescription = document.createTextNode(movieDescription)
+    matchedDescription.appendChild(textDescription)
+
+    matchedMovieTitle.appendChild(matchedDescription)
+
+
+    movieDivTwo.appendChild(matchedMovieTitle)
+    
+    movieDiv.appendChild(movieDivTwo)
+
+    document.getElementById("matches_container").appendChild(movieDiv)
+
+}
+
+
+
+// addMatchesToPage()
+
+
+
+
+/* <div class="row align-items-center">
+    <div class="match_col">
+        <aside class="match_image">
+            <img src="./images/ikiru.jpg" width="200" height="300">
+            </aside>
+            <div class="match_text">
+                <h2>Ikiru (1952)</h2>
+                <h5>Directed by: Akira Kurosawa</h5>
+                <h6>Lorem Ipsum.</h6>
+                <button type="button" class="btn btn-dark">More info.</button>
+            </div>
+          </div>
+    </div> */
+
+
+
+async function hello() {
+    return "hello"
 };
+
+hello().then((value) => console.log(value))
+
+// syncMovies.then((value) => console.log(value))
--- scripts/swipeFunctions.js
+++ scripts/swipeFunctions.js
@@ -98,11 +98,9 @@ var displayNextMovie = () => {
     var movieDescription = movies[movieIndex].data().description;
     var movieImageURL = movies[movieIndex].data().image_url;
 
-    console.log("imageURL" + movieImageURL)
-
     document.getElementById('displayed-movie-image').setAttribute("src", movieImageURL);
 
-    document.getElementById('displayed-movie').innerHTML = movieTitle;
+    // document.getElementById('displayed-movie').innerHTML = movieTitle;
     document.getElementById('displayed-movie-small').innerHTML = movieTitle;
     document.getElementById('displayed-director').innerHTML = movieDirector;
     document.getElementById('displayed-year').innerHTML = movieYear;
@@ -112,8 +110,82 @@ var displayNextMovie = () => {
     console.log(movieTitle);
 }
 
-yes.onclick = displayNextMovie;
-no.onclick = displayNextMovie;
+
+var approveMovie = () => {
+    //document.getElementById('yes').addEventListener('click', function () {
+    
+    if (movieIndex < movies.length) {
+        console.log("length.movies" + movies.length)
+
+        var movieTitle = movies[movieIndex].data().title;
+        var movieDirector = movies[movieIndex].data().director;
+        var movieYear = movies[movieIndex].data().year;
+        var movieDescription = movies[movieIndex].data().description;
+        var movieImageURL = movies[movieIndex].data().image_url;
+
+        movieIndex++;
+
+        firebase.auth().onAuthStateChanged(function (user) {
+            db.collection("userstwo").doc(user.uid).get().then(function (doc) {
+                console.log(user.uid)
+                console.log(doc.data())
+                console.log(doc.data().name)
+    
+                var currentUser = db.collection("userstwo").doc(user.uid)
+
+                if (movieIndex <= 1) {
+                    var movieTitle = movies[movieIndex].data().title;
+                } else {
+                    var movieTitle = movies[movieIndex - 2].data().title;
+                }
+                
+    
+                currentUser.update({
+                    desired_movies: firebase.firestore.FieldValue.arrayUnion(movieTitle)
+                })
+            })
+        })
+
+    } else {
+        var movieTitle = ""
+        var movieDirector = "You've gone through all the movies!"
+        var movieYear = "Why don't you go see your group's matches?"
+        var movieDescription = "Here's a photo of a dog if you want to just chill..."
+        var movieImageURL = "https://firebasestorage.googleapis.com/v0/b/cineder-3be64.appspot.com/o/movie_posters%2Fnomoremovies.png?alt=media&token=0adb5278-6d72-42c1-83cf-855e932b8eae"
+    }
+
+    document.getElementById('displayed-movie-image').setAttribute("src", movieImageURL);
+
+    // document.getElementById('displayed-movie').innerHTML = movieTitle;
+    document.getElementById('displayed-movie-small').innerHTML = movieTitle;
+    document.getElementById('displayed-director').innerHTML = movieDirector;
+    document.getElementById('displayed-year').innerHTML = movieYear;
+    document.getElementById('displayed-description').innerHTML = movieDescription;
+
+}
+
+var disapproveMovie = () => {
+    //document.getElementById('yes').addEventListener('click', function () {
+    var movieTitle = movies[movieIndex].data().title;
+    var movieDirector = movies[movieIndex].data().director;
+    var movieYear = movies[movieIndex].data().year;
+    var movieDescription = movies[movieIndex].data().description;
+    var movieImageURL = movies[movieIndex].data().image_url;
+
+    document.getElementById('displayed-movie-image').setAttribute("src", movieImageURL);
+
+    // document.getElementById('displayed-movie').innerHTML = movieTitle;
+    document.getElementById('displayed-movie-small').innerHTML = movieTitle;
+    document.getElementById('displayed-director').innerHTML = movieDirector;
+    document.getElementById('displayed-year').innerHTML = movieYear;
+    document.getElementById('displayed-description').innerHTML = movieDescription ;
+
+    movieIndex++;
+    console.log(movieTitle);
+}
+
+yes.onclick = approveMovie;
+no.onclick = disapproveMovie;
 
 
 function getMovies() {
@@ -186,9 +258,9 @@ printUserEmail();
 /* ------------------------------ HTML Re-Direct Functions ------------------------------ */
 
 
-document.getElementById(id = "swipeToMatchesButton").onclick = function () {
-    location.href = "./matches.html";
-};
+// document.getElementById(id = "swipeToMatchesButton").onclick = function () {
+//     location.href = "./matches.html";
+// };
 
 
 
