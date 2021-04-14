ae9d3ed54dfe197004c84412ac7c1d6addababdb
Martin Gatchev <mgatchev@gmail.com>
2021-04-13 21:43:24 -0700

    added ordering to matchesFucntion.js

--- scripts/matchesFunctions.js
+++ scripts/matchesFunctions.js
@@ -11,6 +11,26 @@ var movieIndex = 0;
 // };
 
 
+/* ------------------------------ Sort Object Functions ------------------------------ */
+
+function sort_object(obj) {
+    items = Object.keys(obj).map(function(key) {
+        return [key, obj[key]];
+    });
+    items.sort(function(first, second) {
+        return second[1] - first[1];
+    });
+    sorted_obj={}
+    $.each(items, function(k, v) {
+        use_key = v[0]
+        use_value = v[1]
+        sorted_obj[use_key] = use_value
+    })
+    return(sorted_obj)
+} 
+
+
+
 /* ------------------------------ Movie Display Functions ------------------------------ */
 
 // Find the current users group name, pass this information to findAllUsers function
@@ -39,20 +59,24 @@ function findAllUsers(currentGroup){
     .get()
     .then((querySnapshot) => {
 
+        let total_users = 0;
+
         var allMoviesList = []
         querySnapshot.forEach((doc) => {
             // doc.data() is never undefined for query doc snapshots
             // console.log(doc.id, " => ", doc.data());
             allMoviesList.push(doc.data().desired_movies)
-            // console.log(trial_list)     
+            // console.log(trial_list)
+            total_users ++     
         });
 
-        sortAndMatch(allMoviesList)
+        console.log("total_users" + total_users)
+        sortAndMatch(allMoviesList, total_users)
     })
 }
 
 // Combine the list of lists into one large list containing every single element
-function sortAndMatch(allMoviesList){
+function sortAndMatch(allMoviesList, total_users){
 
     var combinedMovies = [];
 
@@ -74,9 +98,14 @@ function sortAndMatch(allMoviesList){
 
     });
 
+    var sortedMovies = sort_object(countedMovies)
+
+    // console.log("sortedMovies")
+    // console.log(sortedMovies)
+
 // maybe pass this object to one final function that creates and displays the DOM's?
-    console.log(countedMovies);
-    getMovies(countedMovies) 
+    // console.log(countedMovies);
+    getMoviesTwo(sortedMovies, total_users) 
 
 }
 
@@ -92,19 +121,59 @@ function sortAndMatch(allMoviesList){
 // 5. Did not alter addMatchesPage() at all
 //  Test it out with a few diff groups and see if it works for you too. Will have to add in some sort of rank?
 
-function getMovies(countedMovies) {
+function getMovies(sortedMovies, total_users) {
     db.collection("movies")
         .get()
         .then(function (snap) {
-            // console.log(countedMovies)
+            console.log(sortedMovies)
             movies = snap.docs;
+            console.log(movies)
             snap.forEach(element => {
-                if (countedMovies.hasOwnProperty(element.data().title)) {
-                    addMatchesToPage(element)
+                if (sortedMovies.hasOwnProperty(element.data().title)) {
+                    let totalLikes = sortedMovies[element.data().title];
+                    let percentLikes = totalLikes / total_users
+                    addMatchesToPage(element, percentLikes)
                 }
             });
         })
 }
+
+
+
+function getMoviesTwo(sortedMovies, total_users) {
+    db.collection("movies")
+        .get()
+        .then(function (snap) {
+            console.log(sortedMovies)
+            movies = snap.docs;
+            console.log(movies)
+
+
+
+            for (var key in sortedMovies) {
+                // check if the property/key is defined in the object itself, not in parent
+                if (sortedMovies.hasOwnProperty(key)) {           
+                    console.log(key, sortedMovies[key]);
+                    snap.forEach(element => {
+                        if (element.data().title == key) {
+                            let totalLikes = sortedMovies[element.data().title];
+                            let percentLikes = totalLikes / total_users
+                            addMatchesToPage(element, percentLikes)
+                        }
+                    })
+                }
+            }
+
+            // snap.forEach(element => {
+            //     if (sortedMovies.hasOwnProperty(element.data().title)) {
+            //         let totalLikes = sortedMovies[element.data().title];
+            //         let percentLikes = totalLikes / total_users
+            //         addMatchesToPage(element, percentLikes)
+            //     }
+            // });
+        })
+}
+
 
 // getMovies();
 
@@ -169,8 +238,9 @@ function addMovieToPage() {
 }
 
 
-function addMatchesToPage(movieVariable) {
+function addMatchesToPage(movieVariable, percentLikes) {
 
+    console.log("percentLikes inside addMatches" + percentLikes)
 
     var movieTitle = movieVariable.data().title;
     var movieDirector = movieVariable.data().director;
@@ -178,7 +248,8 @@ function addMatchesToPage(movieVariable) {
     var movieDescription = movieVariable.data().description;
     var movieImageURL = movieVariable.data().image_url;
 
-    console.log(movieTitle)
+
+    
 
     var movieDiv = document.createElement("div");
     movieDiv.setAttribute("class", "row align-items-center");
@@ -207,7 +278,13 @@ function addMatchesToPage(movieVariable) {
     var textDirector = document.createTextNode(movieDirector)
     matchedDirector.appendChild(textDirector)
 
+    var matchPercentage = document.createElement("h5")
+    var stringPercent = percentLikes * 100
+    var textMatch = document.createTextNode(stringPercent + "% Match")
+    matchPercentage.appendChild(textMatch)
+
     matchedMovieTitle.appendChild(matchedDirector)
+    matchedMovieTitle.appendChild(matchPercentage)
 
     // Description
     var matchedDescription = document.createElement("h6")
@@ -220,6 +297,10 @@ function addMatchesToPage(movieVariable) {
     movieDivTwo.appendChild(matchedMovieTitle)
     
     movieDiv.appendChild(movieDivTwo)
+
+    if (percentLikes == 1) {
+        movieDiv.style.border = "5px solid gold"
+    }
 
     document.getElementById("matches_container").appendChild(movieDiv)
 
