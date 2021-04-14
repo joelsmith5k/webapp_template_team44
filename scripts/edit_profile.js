d98b2371f30f36cce2c3f4712bfa189a3ee126e5
KAZUMA_KUNOGI <kunogikaz@gmail.com>
2021-04-14 16:11:06 -0700

    updated profile

--- scripts/edit_profile.js
+++ scripts/edit_profile.js
@@ -1,31 +1,25 @@
 function getFormInputs() {
-    document.getElementById("save").addEventListener('click', function () {
+    document
+      .getElementById("save")
+      .addEventListener("click", function () {
         firebase.auth().onAuthStateChanged(function (user) {
+          // get various values from the form
+          var name = document.getElementById("userName").value;
+          var aboutme = document.getElementById("aboutMe").value;
+          var country = document.getElementById("country").value;
+          var city = document.getElementById("city").value;
 
-            // get various values from the form
-            var name = document.getElementById("user-name").value;
-            var greetingMessage = document.getElementById("greeting").value;
-            var country = document.getElementById("country").value;
-            var city = document.getElementById("city").value;
-            var user = authResult.user;
-            // if (authResult.additionalUserInfo.isNewUser) {
-            //     db.collection("usersthree").doc(user.uid).set({
-            //         name: name,
-            //         greetingMessage: greeting,
-            //         country: country,
-            //         city: city
-            //     })
+          db.collection("userstwo").doc(userstwo.uid).update({
+            name: name,
+            aboutme: aboutme, // from textarea
+            country: country,//from textarea
+            city: city, //from checkbox
+          });
+          setTimeout(function () {
+            window.location.href = "profile.html"; // this delay allows it to write to DB before redirect
+          }, 2000);
+        });
+      });
+  }
 
-            db.collection("usersthree")
-                .doc(user.uid)
-                .collection("items")
-                .add({
-                    "name": name,   //from text field
-                    "greetingMessage": greeting,     //from checkbox
-                    "country": country,      //from checkbox
-                    "city": city
-                })
-        })
-    })
-}
-getFormInputs();
+  getFormInputs();
