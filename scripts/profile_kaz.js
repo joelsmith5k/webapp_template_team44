0a0c06adb07b0c65aacfab13e2b701493dd2e8df
KAZUMA_KUNOGI <kunogikaz@gmail.com>
2021-04-16 13:22:51 -0700

    updated my files

--- scripts/profile_kaz.js
+++ scripts/profile_kaz.js
@@ -29,6 +29,8 @@ function showProfile() {
 
                     var amazon = doc.data().amazon;
                     var netflix = doc.data().netflix;
+                    var disney = doc.data().disney;
+                    var hulu = doc.data().hulu;
                     if (amazon) {
                         let amazonPrime = "<h2> Amazon Prime </h2>";
                         $("#streamingservice").append(amazonPrime);
@@ -41,6 +43,50 @@ function showProfile() {
                         let netFlix = "<h2> Net Flix </h2>";
                         $("#streamingservice").append(netFlix);
                         let url = "images/netflix.png";
+                        let image = new Image();
+                        image.src = url;
+                        document.getElementById("streamingservice").appendChild(image);
+                    }
+                    if (disney) {
+                        let disneyPlus = "<h2> disneyPlus </h2>";
+                        $("#streamingservice").append(disneyPlus);
+                        let url = "images/disneyplus.jpeg";
+                        let image = new Image();
+                        image.src = url;
+                        document.getElementById("streamingservice").appendChild(image);
+                    }
+                    if (hulu) {
+                        let huluHulu = "<h2> disneyPlus </h2>";
+                        $("#streamingservice").append(huluHulu);
+                        let url = "images/hulu.png";
+                        let image = new Image();
+                        image.src = url;
+                        document.getElementById("streamingservice").appendChild(image);
+                    }
+
+                    var anime = doc.data().anime
+                    var mystery = doc.data().mystery
+                    var romance = doc.data().romance
+                    if (anime) {
+                        let animation = "<h2> Anime </h2>";
+                        $("#streamingservice").append(animation);
+                        let url = "images/anime.jpeg";
+                        let image = new Image();
+                        image.src = url;
+                        document.getElementById("streamingservice").appendChild(image);
+                    }
+                    if (mystery) {
+                        let mysteryPic = "<h2> Mystery </h2>";
+                        $("#streamingservice").append(mysteryPic);
+                        let url = "images/mystery.jpeg";
+                        let image = new Image();
+                        image.src = url;
+                        document.getElementById("streamingservice").appendChild(image);
+                    }
+                    if (romance) {
+                        let romancePic = "<h2> Romance </h2>";
+                        $("#streamingservice").append(romancePic);
+                        let url = "images/romance.jpeg";
                         let image = new Image();
                         image.src = url;
                         document.getElementById("streamingservice").appendChild(image);
